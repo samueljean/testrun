166b0fb022b02f241542ea5855ad5de45c0769cd
samuel jean <samuel jean>
2017-04-03 15:15:20 -0400

    testing it out

---
+++ sidebar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @package _tk
+ */
+?>
+
+	</div><!-- close .main-content-inner -->
+
+	<div class="sidebar ">
+
+
+
+		</div><!-- close .sidebar-padder -->
